6d0cb725fa2c0a30e0c1db9da1c1c558dafd4cad
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-09-01 12:03:27 +0600

    Navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// import daisyui from "daisyui";
+
+// const config = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//         colors:{
+//             background: "var(--background)",
+//             foreground:"var(--foreground)"
+//         }
+//     },
+//   },
+//   plugins: [daisyui],  // wrap inside array
+//   daisyui: {
+//     themes: ["light"],
+//   },
+// };
+
+// export default config;
